88292bda9664e2c0f1e08bd240867e7f02c0a74c
nora <nora9wang@163.com>
2025-04-10 12:53:07 +0100

    edited statement

--- src/main/java/com/develogical/web/IndexPage.java
+++ src/main/java/com/develogical/web/IndexPage.java
@@ -8,7 +8,7 @@ public class IndexPage extends HtmlPage {
   protected void writeContentTo(PrintWriter writer) {
     writer.println(
         "<h1>Welcome!</h1>"
-            + "<p> Enter a query in the box below please: "
+            + "<p> Enter a query in the box below please, thanks: "
             + "<form><input type=\"text\" name=\"q\" />"
             + "<input type=\"submit\">"
             + "</form>"
